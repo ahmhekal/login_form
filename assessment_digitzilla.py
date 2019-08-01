92539c2967916e00502a8d9d19682b4c332b7c40
ahmhekal <ahmhekal@gmail.com>
2019-08-01 05:42:28 +0200

    adding distance calculator, registration and enhancements

--- assessment_digitzilla.py
+++ assessment_digitzilla.py
@@ -5,13 +5,23 @@ from flask_mail import Mail, Message
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 import os
 import re
-import logging
-from werkzeug.utils import secure_filename
-#from pydrop.config import config
+import requests
+from math import cos, asin, sqrt
+
+#API key for google maps
+API_KEY="AIzaSyBVQAVS4Fkx46IuvQy6qDA5KrMjB5kSKFc"
 
 #variables for browser and host information
 browser_and_agent_info, browser_name, host, host_url = 0 ,0, 0, 0
 
+#distance between two points on maps
+def distance(lat1, lon1, lat2, lon2):
+    p = 0.017453292519943295     #Pi/180
+    a = 0.5 - cos((lat2 - lat1) * p)/2 + cos(lat1 * p) * cos(lat2 * p) * (1 - cos((lon2 - lon1) * p)) / 2
+    return 12742 * asin(sqrt(a)) #2*R*asin..
+
+
+#Server Side Validation for email and passwrod
 def is_valid_email(email):
     if len(email) > 7:
         return bool(re.match("^.+@(\[?)[a-zA-Z0-9-.]+.([a-zA-Z]{2,3}|[0-9]{1,3})(]?)$", email))
@@ -34,7 +44,6 @@ def verify_reset_token(token):
     except:
         return None
     return user_id
-
 
 
 def get_session():
@@ -70,9 +79,7 @@ app.config['MYSQL_DB'] = 'flaskdatabase'
 
 mysql = MySQL(app)
 
-
 app.config['SECRET_KEY']  = '5f3e5b6f4c33f8e5ef8b9c24187b3f1c' #any random token for security
-
 
 @app.route('/', methods=['GET','POST'])
 @app.route('/login', methods=['GET','POST'])
@@ -82,8 +89,6 @@ def login():
     browser_name = request.user_agent.browser
     host = request.host
     host_url=request.host_url
-
-    print('browser_and_agent_info=' + str(browser_and_agent_info), 'browser_name = '+str(browser_name), 'host=' + str(host), 'host_url='+ str(host_url))
 
     if request.method =='POST':
         user_details = request.form
@@ -160,27 +165,46 @@ please note that this link will expire after 30 minutes.
 
     mail.send(msg)
 
-@app.route('/reset_password', methods=['GET','POST'])
-def reset_password():
 
+
+@app.route('/register', methods=['GET','POST'])
+def register():
     if request.method =='POST':
         user_details = request.form
         email = user_details['email']
+        name = user_details['name']
+        password = user_details['password']
 
+        try :
+            cur = mysql.connection.cursor()
+            cur.execute("INSERT INTO users (email, name, password )VALUES( %s, %s,%s );", (email, name, password))
+            user_details = cur.fetchall()
+            mysql.connection.commit()
+            cur.close()
+            return render_template('register_success.html')
+        except:
+            return ('There is an error')
+
+    return render_template('register.html')
+
+
+@app.route('/reset_password', methods=['GET','POST'])
+def reset_password():
+    if request.method =='POST':
+        user_details = request.form
+        email = user_details['email']
         cur = mysql.connection.cursor()
         ret=cur.execute("SELECT * FROM users WHERE email= %s", [email])
         user_details = cur.fetchall()
         mysql.connection.commit()
         cur.close()
 
-        print('correct')
-
         if ret:
             send_email(email)
             return render_template('success2.html', email=email)
         else :
-            return ('Your email is not registered')
-
+            flash(f'Your email is not registered')
+            return render_template('reset_password.html')
     return render_template('reset_password.html')
 
 
@@ -216,6 +240,52 @@ def request_password(token):
 @app.route('/upload', methods=['GET','POST'])
 def upload():
     return render_template('large_file_upload.html')
+
+
+@app.route('/map', methods=['GET','POST'])
+def map():
+    if request.method =='POST':
+        details = request.form
+
+        fir=str(details['source'])
+        sec=str(details['destination'])
+
+        fir = fir.replace(' ', '+')
+        sec = sec.replace(' ', '+')
+
+        response1 = requests.get('https://maps.googleapis.com/maps/api/geocode/json?address='+fir+'&key='+API_KEY)
+        first_place = response1.json()
+        first_flag=0
+        try:
+            loc1=( first_place['results'][0]['geometry']['location'])
+
+        except:
+            first_flag = 1
+
+
+        response1 = requests.get('https://maps.googleapis.com/maps/api/geocode/json?address='+sec+'&key='+API_KEY)
+        second_place = response1.json()
+        second_flag=0
+        try:
+            loc2=(second_place['results'][0]['geometry']['location'])
+        except:
+            second_flag=1
+
+
+        if not(first_flag or second_flag):
+
+            dist=get_distance(loc1['lat'], loc1['lng'], loc2['lat'], loc2['lng'])
+            dist=str(dist)
+            return render_template('map_result.html', dist=dist)
+
+        else:
+            dist=distance(30.1255015, 31.3198485, 31.2240349, 30.0949713)
+            dist=str(dist)
+            flash('API limit.. we will show you the distance between Cairo and Alexandria instead')
+            return render_template('map_result.html', dist=dist)
+
+    return render_template('google_maps.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
